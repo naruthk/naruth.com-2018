1b66f36803fe78502c52017dcb6842cc91d986ff
Naruth Kongurai <contact@naruthk.com>
2018-02-03 15:57:50 -0800

    Fixed 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
+import PlantPic from './plant.jpeg';
 
 class NotFoundRoute extends React.Component {
   render() {
     return (
       <div>
-        <Sidebar {...this.props} />
         <div className="content">
           <div className="content__inner">
             <div className="page">
-              <h1 className="page__title">NOT FOUND</h1>
+              <h1 className="page__title">Hmm...</h1>
               <div className="page__body">
-                <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+                <p><b>The page does not exist.</b> Yikes! Maybe it is under construction? Maybe it never will be? :) But I am sorry for what has happened here.</p>
+                <p>Tell me about this on Twitter: <a href="http://www.twitter.com/naruthkongurai" title="Twitter">@naruthkongurai</a>. That will be helpful! Thank you!</p>
+                <p><img src={PlantPic} alt="Credit: Pexel" /></p>
+                <p>I hope this picture calms your emotions down :)</p>
               </div>
             </div>
           </div>
         </div>
+        <Sidebar {...this.props} />
       </div>
     );
   }
@@ -33,6 +37,7 @@ export const pageQuery = graphql`
         menu {
           label
           path
+          description
         }
         author {
           name
